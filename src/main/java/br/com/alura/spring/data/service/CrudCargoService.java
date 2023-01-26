6dfbf6c8b202948b7f9d1747233357731cc1d3c1
Flavia-Duran <duranpepeu@gmail.com>
2023-01-26 08:52:52 -0300

    Adicionando a opção de atualizar um dado na tabela

--- src/main/java/br/com/alura/spring/data/service/CrudCargoService.java
+++ src/main/java/br/com/alura/spring/data/service/CrudCargoService.java
@@ -10,6 +10,7 @@ import br.com.alura.spring.data.repository.CargoRepository;
 @Service
 public class CrudCargoService {  
 	
+	private Boolean system = true;
 	private final CargoRepository cargoRepository;
 
 	
@@ -18,8 +19,29 @@ public class CrudCargoService {
 	}
 
 	public void inicial (Scanner scanner) {
-		salvar(scanner);
-}
+		while(system) { 
+			System.out.println("Qual acao voce quer executar");
+		
+		System.out.println("0-Sair");
+		System.out.println("1- salvar");
+		System.out.println("2- editar");
+		
+		int action = scanner.nextInt();
+		
+		switch(action) {
+		case 1:
+			salvar(scanner);
+			break;
+		case 2:
+			atualizar(scanner);
+			break;
+			
+		default: 
+			system = false;
+			break;
+				}
+			}
+		}
 	
 	public void salvar(Scanner scanner) {
 		System.out.println("Descricao do cargo");
@@ -28,4 +50,18 @@ public class CrudCargoService {
 		cargo.setDescricao(descricao);
 		cargoRepository.save(cargo);
 		System.out.println("Salvo");}
-}
+
+
+	private void atualizar(Scanner scanner) {
+		System.out.println("Id");
+		int id = scanner.nextInt();
+		System.out.println("Descricao do cargo");
+		String descricao = scanner.next();
+		
+		Cargo cargo = new Cargo();
+		cargo.setId(id);
+		cargo.setDescricao(descricao);
+		cargoRepository.save(cargo);
+		System.out.println("atualizado com sucesso!");
+		
+	}}
